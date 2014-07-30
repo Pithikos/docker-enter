cda60826774ce29a635138d24de9099db8402420
pithikos <manossef@hotmail.com>
2014-07-30 17:29:07 +0100

    Initial commit

--- docker-enter.c
+++ docker-enter.c
@@ -1,7 +1,7 @@
 /*
  * Author: Johan Hanssen Seferidis
  * 
- * Docker-enter
+ * docker-enter
  * 
  * */
 
@@ -24,7 +24,6 @@
 
 #define VERSION "docker-enter v0.1"
 #define DEFAULT_SHELL "/bin/sh"
-
 
 
 static char  dir [PATH_MAX]       = {'\0'};	// Working directory
@@ -75,36 +74,41 @@ static pid_t container_pid(char* cont_ID) {
 	
 	pid_t cont_pid = -1;
 	char output[4096];
-	
 	int pip[2];
-	  pid_t pid;
-
-	  if (pipe(pip)==-1)
-	    perror("Pipe");
-
-	  if ((pid = fork()) == -1)
-	    perror("Fork");
-
-	  if(pid == 0) { // child
-	    dup2 (pip[1], STDOUT_FILENO);
-	    close(pip[0]);
-	    //close(STDERR_FILENO);
+	pid_t pid;
+	
+	if (pipe(pip)==-1)
+		perror("Pipe");
+	
+	if ((pid = fork()) == -1)
+		perror("Fork");
+	
+	if(pid == 0) { // child
+		dup2 (pip[1], STDOUT_FILENO);
+		close(pip[0]);
 		execlp( "docker", "docker", "inspect", "--format", "{{.State.Pid}}", cont_ID, (char*)0 );
-		exit(0);
-	  } else {       // parent
-		wait(NULL);
-	    close(pip[1]);
-	    read(pip[0], output, sizeof(output));
-	    printf("DOCKER OUTPUT: %s\n", output);
-	    sscanf(output, "%ld", (long int*)&cont_pid);
-	  }
+		exit(1);
+	} else {       // parent
+		int status=-10;
+		wait(&status);
+		if (status != 0){
+			fprintf(stderr, "Docker terminated with error code %d\n", status);
+			exit(1);
+		}
+		close(pip[1]);
+		read(pip[0], output, sizeof(output));
+		sscanf(output, "%ld", (long int*)&cont_pid);
+	}
 
 	return cont_pid;
 }
 
 
 
-
+/* 
+ * Prints on screen the usage of command
+ * 
+ * */
 static void usage_print()
 {
 	printf("Usage: %s [options] <container ID>\n", program_invocation_short_name);
@@ -117,8 +121,7 @@ static void usage_print()
 
 
 /*
- * Parses the command given by the user (uses optget)
- * 
+ * Parses the command given by the user.
  * In practice it changes global variables.
  * 
  * */
@@ -177,10 +180,7 @@ static void parse_command(int argc, char *argv[]){
 		fprintf(stderr, "No container ID given. Use -h for help\n");
 		exit(1);
 	}
-	printf("user: %s\ndir: %s\ncontainer: %s\n", user, dir, cont_ID);
-	
-	// Get pid of container
-	cont_pid = container_pid(argv[1]);
+	cont_pid = container_pid(cont_ID);
 	if ( cont_pid < 0) {
 		exit(1);
 	}
@@ -200,20 +200,19 @@ static void open_namespace(char* ns_name) {
 	while ( ns->name != NULL ) {
 		if (ns->name == ns_name)
 		{
-			printf("Will open: %s\n", ns->name);
 			char pathbuf[PATH_MAX];
 			int fd = -1;
 			snprintf(pathbuf, sizeof(pathbuf), "/proc/%u/%s", cont_pid, ns->file);
 			if ((fd = open(pathbuf, O_RDONLY)) == -1) {
-				perror("Trying to open namespace");
+				fprintf(stderr, "Trying to open namespace '%s': %s\n", ns->file, strerror(errno));
 				exit(1);
 			}
 			
-			if (setns(fd, ns->type) < 0) {
-				perror("Trying to set namespace");
+			if (setns(fd, 0) < 0) {
+				fprintf(stderr, "Trying to set namespace '%s': %s\n", ns->file, strerror(errno));
 				exit(1);
 			}
-			return 0;
+			return;
 		}
 		ns++;
 	}
@@ -228,7 +227,7 @@ int main (int argc, char *argv[]) {
 	parse_command(argc, argv);
 	
 	// Open namespaces
-	open_namespace("user");
+	//open_namespace("user");
 	open_namespace("ipc");
 	open_namespace("uts");
 	open_namespace("net");
@@ -237,6 +236,10 @@ int main (int argc, char *argv[]) {
 
 	// Change directory
 	chdir(dir);
+	
+	// Change user
+	if (strlen(user)>0)
+		execlp( "su", "su", user, (char*)0 );
 
 	// Run cell
 	exec_shell();
